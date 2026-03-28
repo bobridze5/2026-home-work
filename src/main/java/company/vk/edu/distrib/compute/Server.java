798eac5be71ba80abfb8384b82d536413a50f295
Nikita <troshin.nksr@gmail.com>
2026-03-28 23:44:04 +0300

    revert Server.java

--- src/main/java/company/vk/edu/distrib/compute/Server.java
+++ src/main/java/company/vk/edu/distrib/compute/Server.java
@@ -1,32 +1,15 @@
 package company.vk.edu.distrib.compute;
 
-//import company.vk.edu.distrib.compute.nihuaway00.NihuawayKVService;
-//import company.vk.edu.distrib.compute.nihuaway00.NihuawayKVServiceFactory;
-
 import module java.base;
-import company.vk.edu.distrib.compute.dummy.DummyKVServiceFactory;
+import company.vk.edu.distrib.compute.mandesero.KVServiceFactoryImpl;
 import org.slf4j.LoggerFactory;
 
 public class Server {
-    //    public static void main(String[] args) throws IOException {
-    //        var log = LoggerFactory.getLogger("server");
-    //        var port = 8080;
-    //        KVService storage = new NihuawayKVServiceFactory().create(port);
-    //        storage.start();
-    //        log.info("Server started on port {}", port);
-    //        Runtime.getRuntime().addShutdownHook(new Thread(storage::stop));
-    //
-    //        try {
-    //            Thread.currentThread().join();
-    //        } catch (InterruptedException e) {
-    //            log.warn("error", e);
-    //        }
-    //    }
 
     void main() throws IOException {
         var log = LoggerFactory.getLogger("server");
         var port = 8080;
-        KVService storage = new DummyKVServiceFactory().create(port);
+        KVService storage = new KVServiceFactoryImpl().create(port);
         storage.start();
         log.info("Server started on port {}", port);
         Runtime.getRuntime().addShutdownHook(new Thread(storage::stop));
